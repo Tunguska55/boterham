ec540101e03f6514a25c1807a7b111a7c8b89a0a
Jonathan Quintanilla <quintanillajon@gmail.com>
2022-03-31 17:27:18 -0400

    Adding the beginning stages of the app

--- boterham/__init__.py
+++ boterham/__init__.py
@@ -1,1 +1,8 @@
-__version__ = '0.1.0'
+# __version__ = '0.1.0'
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+from boterham import routes
---
+++ boterham/routes.py
@@ -0,0 +1,8 @@
+from turtle import title
+from boterham import app
+from flask import render_template
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template("index.html", title="Exam Study")
