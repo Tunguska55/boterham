6ede70f53051f03ea2ba7104da946f36034585b8
Jonathan Quintanilla <quintanillajon@gmail.com>
2022-04-16 19:14:07 -0400

    Added setup for dev and prod

--- boterham/__init__.py
+++ boterham/__init__.py
@@ -1,8 +1,17 @@
 # __version__ = '0.1.0'
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import ProdConfig, DevConfig, TestConfig
 
 app = Flask(__name__)
+
+if app.config.get("FLASK_ENV") == "production":
+    app.config.from_object(ProdConfig)
+elif app.config.get("FLASK_ENV") == "testing":
+    app.config.from_object(TestConfig)
+else:
+    app.config.from_object(DevConfig)
+
 db = SQLAlchemy(app)
 
 from boterham import routes
